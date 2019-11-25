2999da67f4ded91cbe6910972cdb02da81913fa3
yuvallhv <yuv.lahav@gmail.com>
2019-11-25 22:37:16 +0200

    added functions: launch, terminate and tested usage

--- Assignment1/src/main/java/EC2Handler.java
+++ Assignment1/src/main/java/EC2Handler.java
@@ -11,6 +11,7 @@ import com.amazonaws.services.ec2.model.InstanceType;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.model.DescribeTagsRequest;
@@ -26,25 +27,53 @@ public class EC2Handler {
     private static final String TAG_MANAGER = "TAG_MANAGER";
     private static final String TAG_WORKER = "TAG_WORKER";
 
-    public static void launch() {
+    public static AmazonEC2 connectEC2(){
         AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(new ProfileCredentialsProvider().getCredentials());
-        AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard()
+        return  AmazonEC2ClientBuilder.standard()
                 .withCredentials(credentialsProvider)
                 .withRegion(Regions.US_EAST_1)
                 .build();
+    }
 
+    public static List<Instance> launchEC2Instance(AmazonEC2 ec2) throws Exception {
         try {
-            // Basic 32-bit Amazon Linux AMI 1.0 (AMI Id: ami-b66ed3de)
             RunInstancesRequest request = new RunInstancesRequest(AMI, 1, 1);
             request.setInstanceType(InstanceType.T2Micro.toString());
             List<Instance> instances = ec2.runInstances(request).getReservation().getInstances();
             System.out.println("Launch instances: " + instances);
+            System.out.println("You launched: " + instances.size() + " instances");
+            return instances;
 
         } catch (AmazonServiceException ase) {
-            printException(ase);
+            System.out.println("Caught Exception: " + ase.getMessage());
+            System.out.println("Response Status Code: " + ase.getStatusCode());
+            System.out.println("Error Code: " + ase.getErrorCode());
+            System.out.println("Request ID: " + ase.getRequestId());
+            return null;
         }
     }
 
+    public static boolean terminateEC2Instance(AmazonEC2 ec2Client, String instanecID) throws Exception {
+        try {
+            TerminateInstancesRequest terminateInstancesRequest = new TerminateInstancesRequest()
+                    .withInstanceIds(instanecID);
+            ec2Client.terminateInstances(terminateInstancesRequest)
+                    .getTerminatingInstances()
+                    .get(0)
+                    .getPreviousState()
+                    .getName();
+            System.out.println("The Instance is terminated with id: "+ instanecID);
+            return true;
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught Exception: " + ase.getMessage());
+            System.out.println("Response Status Code: " + ase.getStatusCode());
+            System.out.println("Error Code: " + ase.getErrorCode());
+            System.out.println("Request ID: " + ase.getRequestId());
+            return false;
+        }
+
+    }
 
     public static void describeInstances(AmazonEC2 ec2) {
         boolean done = false;
@@ -63,16 +92,14 @@ public class EC2Handler {
                         List<TagDescription> tags = tagResult.getTags();
                         boolean isManagerInst = isMangaer(tags);
 
-                        // The app should use the existing manager node
-                        if (isManagerInst) {
-                            // TODO
-                        }
-                        // The app should start the manager node
-                        else {
-                            // TODO
-                        }
-
-
+//                        // The app should use the existing manager node
+//                        if (isManagerInst) {
+//                            // TODO
+//                        }
+//                        // The app should start the manager node
+//                        else {
+//                            // TODO
+//                        }
 
                         System.out.printf(
                                 "Found instance with id %s, " +
--- Assignment1/src/main/java/LocalApplication.java
+++ Assignment1/src/main/java/LocalApplication.java
@@ -1,7 +1,19 @@
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.Instance;
+
+import java.util.List;
+
 public class LocalApplication {
 
 
-
+    public static void main(String[] args) throws Exception {
+        AmazonEC2 ec2= EC2Handler.connectEC2();
+        List<Instance> myInstances = EC2Handler.launchEC2Instance(ec2);
+        if(myInstances != null){
+            String instanceIdToTerminate = myInstances.get(0).getInstanceId();
+            EC2Handler.terminateEC2Instance(ec2, instanceIdToTerminate);
+        }
+    }
 
 
 }
