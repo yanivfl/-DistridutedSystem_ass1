9a53bc86d82acf6c4ac6178233b041fc65cb13d6
yanivfl <yanivfl@post.bgu.ac.il>
2019-12-02 21:07:05 +0200

    updated Constants updated Worker updated Messages

--- Assignment1/src/main/java/apps/Constants.java
+++ Assignment1/src/main/java/apps/Constants.java
@@ -12,8 +12,6 @@ public class Constants {
 
     public static final String AMI = "ami-b66ed3de";
     public static final String TAG = "tag";
-    public static final String TAG_MANAGER = "TAG_MANAGER";
-    public static final String TAG_WORKER = "TAG_WORKER";
     public static final String REVIEWS= "reviews";
     public static final String REVIEW= "review";
     public static final String TEXT= "text";
@@ -34,6 +32,10 @@ public class Constants {
     public static final String WORKERS_TO_MANAGER_QUEUE = "Workers2ManagerQueue";
 
     public static final String MANAGER_TO_WORKERS_QUEUE = "Manager2WorkersQueue";
+
+    public enum INSTANCE_TAG {
+        TAG_MANAGER, TAG_WORKER
+    }
 
 
     public enum TAGS {
